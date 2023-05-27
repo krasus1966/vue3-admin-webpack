16e958c99c37be682a3bfd3129047d03454745ec
chenxu <2134123985@qq.com>
2023-05-27 12:51:54 +0800

    fix(all): 换cz-custom
    换cz-custom

--- .cz-config.js
+++ .cz-config.js
@@ -9,25 +9,21 @@ module.exports = {
     { value: 'test', name: 'test:     增加测试' },
     { value: 'chore', name: 'chore:    构建过程或辅助工具的变动' },
     { value: 'revert', name: 'revert:   回退' },
-    { value: 'build', name: 'build:      打包' }
+    { value: 'build', name: 'build:   打包' }
   ],
-  scopes: [
-    { name: 'accounts' },
-    { name: 'admin' },
-    { name: 'exampleScope' },
-    { name: 'changeMe' }
-  ],
+
   // 消息步骤
   messages: {
-    type: '请选择提交类型',
+    type: '请选择提交类型：',
     // scope: '选择一个scope (可选):',
-    customScope: '请输入修改范围(可选)',
-    subject: '请简要描述提交(必填)',
-    body: '请输入详细描述(可选)',
-    breaking: '列举重大变化(可选)',
-    footer: '请输入要关闭的issue(可选)',
-    confirmCommit: '确认使用以上信息提交？(y/n/e/h)'
+    customScope: '请输入修改范围(可选)：',
+    subject: '请简要描述提交(必填)：',
+    body: '请输入详细描述(可选)：',
+    breaking: '列举重大变化(可选):',
+    footer: '请输入要关闭的issue(可选)：',
+    confirmCommit: '确认使用以上信息提交？(y/n/e/h)：'
   },
+  askAnswered: true,
   allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix'],
   // // limit subject length
